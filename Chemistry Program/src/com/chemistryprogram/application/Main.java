5ae6d6e1463d697c517e9c39f90a458a2c906fa1
sanyemaa <sanyemaa@gmail.com>
2017-05-05 13:38:39 -0400

    main.java with loops/exception handling
    Still working on a few stuff, but this should be good for exception handling. I might change loops to do-while. I also cleaned up some other stuff--details inside

--- Chemistry Program/src/com/chemistryprogram/application/Main.java
+++ Chemistry Program/src/com/chemistryprogram/application/Main.java
@@ -1,36 +1,34 @@
 package com.chemistryprogram.application;
 
-import com.chemistryprogram.elements.*;
-import com.chemistryprogram.Assessment.*;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import com.chemistryprogram.Assessment.MultipleChoiceTestAssessment;
+import com.chemistryprogram.elements.Elements;
 
-
-import java.io.*;
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Created by Faraan on 1/11/2017.
+ * Created by Faraan on 1/11/2017 & fudged by Yemeni thereafter.
  */
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in); //A2MA: got rid of local assignments to the field
 
-    public static void main(String[] args) throws  IOException {
-
+    public static void main(String[] args) throws IOException {
 
         Elements elementsInstance = new Elements();
         ArrayList<Elements> elements = elementsInstance.getElements();
 
-        System.out.println(elementsInstance.getElements().get(1));
+//        System.out.println(elementsInstance.getElements().get(1));
 
         boolean quit = false;
-        printMainMenu();
+        int elementChoice;
+        String filename;
+        System.out.println("Welcome to the Chemistry Program.");
         while (!quit) {
+            printMainMenu();
             System.out.println("Make selection:");
             int selection = 99;
-            scanner = new Scanner(System.in);
             try {
                 selection = scanner.nextInt();
             } catch (InputMismatchException e) {
@@ -44,18 +42,19 @@ public class Main {
                     quit = true;
                     break;
                 case 1:
-
+//                  Convert moles to grams
                     boolean quit1 = false;
-                    while(!quit1) {
-
-                        scanner = new Scanner(System.in);
+                    while (!quit1) {
                         System.out.println("Which element? (Atomic Number)");
-                        int elementChoice;
+//                        A2MA: trying to show user available elements
+//                        System.out.println("Available Elements:");
+//                        for(int i = 0; i < elements.size(); i++){
+//                            System.out.println(elements.get(i));
+//                        }
                         try {
                             elementChoice = scanner.nextInt();
                             quit1 = true;
                             boolean quit2 = false;
-
                             while (!quit2) {
                                 try {
                                     if (elementChoice <= elements.size() && elementChoice >= 0) {
@@ -65,6 +64,39 @@ public class Main {
                                         System.out.println("Element does not exist");
                                         quit2 = true;
                                         quit1 = false;
+                                    }
+                                } catch (InputMismatchException f) {
+                                    System.out.println("Invalid input type. Please try again with an available choice.");
+                                } catch (Exception e) {
+                                    System.out.println(e.getMessage());
+                                    System.out.println("Invalid input type wrong message" + "\n");
+                                }
+                            }
+                        } catch (InputMismatchException f) {
+                            System.out.println("Invalid element input type");
+                        } catch (Exception f) {
+                            System.out.println("Some other type of error");
+                        }
+                    }
+                    break;
+                case 2:
+//                  Convert grams to moles
+                    boolean quit2 = false;
+                    while (!quit2) {
+                        System.out.println("Which element? (Atomic Number)");
+                        try {
+                            elementChoice = scanner.nextInt();
+                            quit2 = true;
+                            boolean quit3 = false;
+                            while (!quit3) {
+                                try {
+                                    if (elementChoice <= elements.size() && elementChoice >= 0) {
+                                        elements.get(elementChoice).molesToGrams();
+                                        quit3 = true;
+                                    } else {
+                                        System.out.println("Element does not exist");
+                                        quit3 = true;
+                                        quit2 = false;
                                     }
                                 } catch (InputMismatchException f) {
                                     System.out.println("Invalid input type");
@@ -80,50 +112,59 @@ public class Main {
                         }
                     }
                     break;
-
-                case 2:
-//                    Convert grams to moles
-                    System.out.println("Which element? (Atomic Number)");
-                    int elementChoice2 = scanner.nextInt();
-                    elements.get(elementChoice2).gramsToMoles();
-                    break;
-
                 case 3:
-                    scanner = new Scanner(System.in);
-                    System.out.println("Enter file name.");
-                    String filename = scanner.nextLine();
-                    MultipleChoiceTestAssessment multipleChoiceTestAssessment = new MultipleChoiceTestAssessment(filename + ".txt");
-
-                  printAssessmentMenu();
-
-                        System.out.println("Make selection:");
-                        selection = scanner.nextInt();
-                        switch (selection) {
-                            case 0:
-                                multipleChoiceTestAssessment.gradeTest();
-                                break;
-                            case 1:
-                                multipleChoiceTestAssessment.printTest();
+                    boolean quit3 = false;
+                    while (!quit3) {
+                        System.out.println("Enter file name.");
+                        try {
+                            filename = scanner.nextLine();
+                            MultipleChoiceTestAssessment multipleChoiceTestAssessment = new MultipleChoiceTestAssessment(filename + ".txt");
+                            printAssessmentMenu();
+                            quit3 = true; //for a future invalid submission checking for the test assessment
+                            boolean quit4 = false;
+                            while (!quit4) {
+                                System.out.println("Make selection:");
+                                try {
+                                    selection = scanner.nextInt();
+                                    switch (selection) {
+                                        case 0:
+                                            multipleChoiceTestAssessment.gradeTest();
+                                            quit4 = true;
+                                            break;
+                                        case 1:
+                                            multipleChoiceTestAssessment.printTest();
+                                            quit4 = true;
+                                        default:
+                                            System.out.println("Not an option");
+                                            break;
+                                    }
+                                } catch (InputMismatchException f) {
+                                    System.out.println("Invalid input type");
+                                } catch (Exception e) {
+                                    System.out.println(e.getMessage());
+                                    System.out.println("Invalid input type wrong message" + "\n");
+                                }
+                            }
+                            break;
+                        } catch (InputMismatchException f) {
+                            System.out.println("Invalid element input type");
+                        } catch (Exception f) {
+                            System.out.println("Some other type of error");
                         }
-                    break;
-
-                default:
-                    System.out.println("Not an option");
-                    break;
-
+                    }
             }
         }
+
     }
 
-
-    public static void printMainMenu() {
+    private static void printMainMenu() { //made access private -- suggested by intelliJ
         System.out.println("0 - End program");
         System.out.println("1 - Convert moles to grams");
         System.out.println("2 - Convert grams to moles");
         System.out.println("3 - Assessment");
     }
 
-    public static void printAssessmentMenu() {
+    private static void printAssessmentMenu() { //made access private
         System.out.println("0 - Grade Test");
         System.out.println("1 - Print Test");
     }
